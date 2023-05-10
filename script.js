14a34f8f683deef17cd8b3dccf6eec775f03ee0d
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-10 07:23:46 +0500

    script.js
    events practice

---
+++ script.js
@@ -0,0 +1,31 @@
+// function over {
+//     document.getElementsByid("pera").className="class1"
+//     // document.getElementById("pera").style.backgroundColor=("pink")
+// }
+// function over() {
+//     document.getElementById("pera").className = "bg1";
+//     document.getElementById("pera").style.backgroundColor = "bg1";
+// }
+// function out() {
+//   document.getElementById("pera").className="bg2";
+//   document.getElementById("pera").style.backgroundColor="bg2" ; 
+// }
+
+
+function over() {
+    document.getElementById("h3").className="bg1";
+    document.getElementById("h3").style="bg1"
+}
+function out() {
+    document.getElementById("h3").className="bg1";
+    document.getElementById("h3").style="bg1"
+}
+
+function over() {
+    document.getElementById("pera").className="bg1";
+    document.getElementById("pera").style="bg1"
+}
+function out() {
+    document.getElementById("pera").className="bg2";
+    document.getElementById("pera").style="bg2"
+}
